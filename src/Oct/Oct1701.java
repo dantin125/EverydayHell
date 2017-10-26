a729b6ba76afe7d1c157d13d54ad8d7d29349211
zhangxiaolong <zhangxiaolong@xlauncher.io>
2017-10-26 16:12:46 +0800

    Edit Oct1701

--- src/Oct/Oct1701.java
+++ src/Oct/Oct1701.java
@@ -15,5 +15,22 @@ package Oct;
  */
 
 public class Oct1701 {
-
+	public static void main(String[] args) {
+		System.out.println("   *");
+		System.out.println("  ***");
+		System.out.println(" *****");
+		System.out.println("*******");
+		System.out.println(" *****");
+		System.out.println("  ***");
+		System.out.println("   *");
+	}
+	
+	public static void printStar(int spaceNum, int starNum) {
+		for(int i = 0; i < spaceNum; i++) {
+			System.out.print(" ");
+		}
+		for(int i = 0; i < starNum; i++) {
+			System.out.print("*");
+		}
+	}
 }
